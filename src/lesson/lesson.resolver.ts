4adb6bdcb79c5136bc92bdb8ece7a6076eba1ec5
Oliver Sosa <cundejo85@gmail.com>
2020-06-14 23:17:35 -0300

    Created some queries and mutations over Lesson collection

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,15 +1,24 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
+  @Query(returns => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
   @Query(returns => LessonType)
-  lesson() {
-    return {
-      id: '1234112',
-      name: 'Physics Class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Mutation(returns => LessonType)
+  createLesson(@Args('createLessonInput') createLessonInput: CreateLessonInput) {
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
